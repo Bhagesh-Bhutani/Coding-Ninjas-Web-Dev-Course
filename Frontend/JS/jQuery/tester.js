0c5a28b47a463a9f29c7cbd15b1661521d13f321
Bhagesh-Bhutani <bhutanibhagesh49@gmail.com>
2021-05-30 03:05:15 +0530

    jQuery

---
+++ Frontend/JS/jQuery/tester.js
@@ -0,0 +1,23 @@
+// $('#para1').text("hey");
+// var html = "<button>Submit</button>";
+// $('#para1').html("<button>Submit</button>");
+// $('#para1').html(html);
+// $('#para1').hide();
+// $('#para1').show();
+// document.getElementById('para1').innerHTML = "<button>Submit</button>";
+
+// Modifying css
+
+$('p:nth-child(2)').css('color','red');
+$('p').css({
+    'color': 'blue',
+    'font-size': '2.5rem'
+});
+
+$('#para1').addClass('red blue');
+// $('#para1').toggleClass('red');
+// $('#para1').removeClass('red');
+
+// document.addEventListener('click' , function(e) {
+//     console.log(this);
+// });
