b14af9838a3675c9ceaf2ec9638892ff0f93918a
Bhagesh-Bhutani <bhutanibhagesh49@gmail.com>
2021-06-03 17:46:57 +0530

    AJAX via jQuery methods

--- Frontend/JS/AJAX/script_jquery.js
+++ Frontend/JS/AJAX/script_jquery.js
@@ -8,12 +8,32 @@ function textReplacer(){
 img.attr('onload','textReplacer()');
 
 btn.click((e) => {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get','https://dog.ceo/api/breeds/image/random',true);
-    xhr.onload = function(){
-        var obj = JSON.parse(xhr.response);
-        img.attr('src',obj.message);
-    };
-    xhr.send();
     btn.text('Working on It...');
+
+    // Method 1 : $.ajax() method
+    // $.ajax({
+    //     url: "https://dog.ceo/api/breeds/image/random",
+    //     method: "get",
+    //     success: (data) => {
+    //         console.log("Oh yeahh");
+    //         img.attr('src',data.message);
+    //     }
+    // });
+
+    // Method 2 : $.get() method
+    $.get("https://dog.ceo/api/breeds/image/random",(data) => {
+        console.log('Via jQuery get');
+        img.attr('src',data.message);
+    });
 });
+
+// btn.click((e) => {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('get','https://dog.ceo/api/breeds/image/random',true);
+//     xhr.onload = function(){
+//         var obj = JSON.parse(xhr.response);
+//         img.attr('src',obj.message);
+//     };
+//     xhr.send();
+//     btn.text('Working on It...');
+// });
