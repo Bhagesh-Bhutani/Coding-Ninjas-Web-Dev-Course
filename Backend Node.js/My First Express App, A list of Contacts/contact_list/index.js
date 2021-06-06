bd419d4cb2e77e1104f6cc955ba532abb80d67b0
Bhagesh-Bhutani <bhutanibhagesh49@gmail.com>
2021-06-07 02:46:08 +0530

    Started EJS

---
+++ Backend Node.js/My First Express App, A list of Contacts/contact_list/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const port = 8000;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req,res){
+    return res.render('home');
+});
+
+app.listen(port, function(err){
+    if(err){
+        console.log('Error in running the server',err);
+        return;
+    }
+
+    console.log('Server is running at : http://localhost:' + port);
+});
