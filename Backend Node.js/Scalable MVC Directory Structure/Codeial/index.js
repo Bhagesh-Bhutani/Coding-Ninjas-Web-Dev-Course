527867a84540fe87caeaa6260a1e64e543ca3c49
Bhagesh-Bhutani <bhutanibhagesh49@gmail.com>
2021-06-24 02:25:55 +0530

    Created directory structure

--- Backend Node.js/Scalable MVC Directory Structure/Codeial/index.js
+++ Backend Node.js/Scalable MVC Directory Structure/Codeial/index.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
 
+const port = 8000;
+
+app.listen(port, function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    console.log(`Server is running on port : ${port}`);
+});
